19286ba601846b8e05c80f6afa22e2dabe3f71b6
M-Schoonen <40211284@roctilburg.nl>
2025-05-06 11:55:23 +0200

    Songs in beeld bij library

--- resources/views/library.blade.php
+++ resources/views/library.blade.php
@@ -2,4 +2,13 @@
 
 @section('content')
     <h1>Muziekbibliotheek Pagina</h1>
+
+    <h2>Alle songs</h2>
+
+    <ul>
+        @foreach($songs as $song)
+            <li>{{ $song->song_name }} – {{ $song->author }} ({{ $song->release_year }})</li>
+        @endforeach
+    </ul>
+
 @endsection
